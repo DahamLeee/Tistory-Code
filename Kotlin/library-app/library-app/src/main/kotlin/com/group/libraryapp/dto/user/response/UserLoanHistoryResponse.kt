417995fb8cdbcb03d7a58b44f6e4211183010796
DahamLeee <dlek1011@gmail.com>
2022-09-11 17:12:01 +0900

    feat: [GET] API UserLoanHistory production code

---
+++ Kotlin/library-app/library-app/src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
@@ -0,0 +1,15 @@
+package com.group.libraryapp.dto.user.response
+
+data class UserLoanHistoryResponse(
+    val name: String, // 유저 이름
+    val books: List<BookHistoryResponse> // 대출 기록
+) {
+}
+
+
+data class BookHistoryResponse(
+    val name: String, // 책의 이름
+    val isReturn: Boolean, // 반납 여부
+) {
+
+}
