c8e7a56f8e3e0f5f483fea0352359b27fa952e92
DahamLeee <dlek1011@gmail.com>
2022-09-14 12:33:36 +0900

    feat: change Spring Data JPA JPQL to Querydsl

--- library-app/library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ library-app/library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,14 +1,9 @@
 package com.group.libraryapp.domain.user
 
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
 
     fun findByName(name: String): User?
 
-    @EntityGraph(attributePaths = ["userLoanHistories"])
-    @Query("select u from User u")
-    fun findAllWithHistories(): List<User>
 }
