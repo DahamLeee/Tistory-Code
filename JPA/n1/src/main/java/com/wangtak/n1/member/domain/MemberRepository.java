9a5b9036601df4ded2e3c9e982dd32c275acbd59
DahamLeee <dlek1011@gmail.com>
2022-10-14 14:34:57 +0900

    feat: N + 1 Solved Code [fetch join, @EntityGraph]

--- JPA/n1/src/main/java/com/wangtak/n1/member/domain/MemberRepository.java
+++ JPA/n1/src/main/java/com/wangtak/n1/member/domain/MemberRepository.java
@@ -1,6 +1,21 @@
 package com.wangtak.n1.member.domain;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select m from Member m join fetch m.team")
+    List<Member> findAllUsingFetchJoin();
+
+    @EntityGraph(attributePaths = "team")
+    @Query("select m from Member m")
+    List<Member> findAllUsingJPQLAndEntityGraph();
+
+    @Override
+    @EntityGraph(attributePaths = "team")
+    List<Member> findAll();
 }
--- JPA/n1/src/test/java/com/wangtak/n1/MemberRepositoryTest.java
+++ JPA/n1/src/test/java/com/wangtak/n1/MemberRepositoryTest.java
@@ -11,15 +11,57 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @SpringBootTest
+@Transactional(readOnly = true)
 public class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
-    
+
     @Test
     @DisplayName("멤버 조회 시 추가적인 쿼리 발생 확인")
-    @Transactional(readOnly = true)
     void findAllTest() {
+        List<Member> findMembers = memberRepository.findAll();
+
+        findMembers.forEach(member -> {
+            System.out.println(member.getTeam().getClass());
+        });
+
+        findMembers.forEach(member -> {
+            System.out.println(member.getTeam().getName());
+        });
+    }
+
+    @Test
+    @DisplayName("fetch join 을 사용하여 멤버 조회")
+    void findAllMemberUsingFetchJoinTest() {
+        List<Member> findMembers = memberRepository.findAllUsingFetchJoin();
+
+        findMembers.forEach(member -> {
+            System.out.println(member.getTeam().getClass());
+        });
+
+        findMembers.forEach(member -> {
+            System.out.println(member.getTeam().getName());
+        });
+    }
+
+    @Test
+    @DisplayName("Entity Graph + JPQL 을 사용하여 멤버 조회")
+    void findAllMemberUsingEntityGraphAndJPQL() {
+        List<Member> findMembers = memberRepository.findAllUsingJPQLAndEntityGraph();
+
+        findMembers.forEach(member -> {
+            System.out.println(member.getTeam().getClass());
+        });
+
+        findMembers.forEach(member -> {
+            System.out.println(member.getTeam().getName());
+        });
+    }
+
+    @Test
+    @DisplayName("Spring Data JPA 에서 제공하는 기본 메서드에 EntityGraph 활용")
+    void findAllMemberUsingEntityGraph() {
         List<Member> findMembers = memberRepository.findAll();
 
         findMembers.forEach(member -> {
